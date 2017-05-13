526ff592baf6db3c68fb46ea093dacf6def4bc97
songshuilin <631659521@qq.com>
2017-05-13 18:43:56 +0800

    修改图片的不显示问题

--- app/src/main/java/https/GetPictureAPI.java
+++ app/src/main/java/https/GetPictureAPI.java
@@ -27,22 +27,40 @@ public class GetPictureAPI {
         try {
             String url = getUrlForType(type);
             Document document = Jsoup.connect(url).get();
-            Elements imgs = document.getElementsByTag("img");
+            Elements liboxElemnets =document.getElementsByClass("libox");
 
-            if (imgs != null) {
-                list = new ArrayList<>();
-            }
+            if (liboxElemnets.size()>0) {
+               list = new ArrayList<>();
+           }
 
-            for (int i = 10; i < imgs.size()-10; i++) {
+            for (int i = 10; i <liboxElemnets.size() ; i++) {
                 PictureBean pictureBean = new PictureBean(Parcel.obtain());
-                if (imgs.get(i).hasAttr("alt")) {
-                    String imgTitle = imgs.get(i).attr("alt");//图片描述
-                    String imgUrl = imgs.get(i).attr("src");
-                    pictureBean.setPictureUrl(imgUrl);
-                    pictureBean.setPictureTitle(imgTitle);
-                    list.add(pictureBean);
+                String imgUrl=null;
+                if (liboxElemnets.get(i).getElementsByTag("img").hasAttr("lazysrc")){
+                imgUrl= liboxElemnets.get(i).getElementsByTag("img").attr("lazysrc");
+                }else {
+                imgUrl= liboxElemnets.get(i).getElementsByTag("img").attr("src");
                 }
+                String imgTitle=liboxElemnets.get(i).getElementsByTag("p").text();
+                pictureBean.setPictureUrl(imgUrl);
+                pictureBean.setPictureTitle(imgTitle);
+                list.add(pictureBean);
             }
+
+//            Elements imgs = document.getElementsByTag("img");
+//            if (imgs != null) {
+//                list = new ArrayList<>();
+//            }
+//            for (int i = 10; i < imgs.size()-10; i++) {
+//                PictureBean pictureBean = new PictureBean(Parcel.obtain());
+//                if (imgs.get(i).hasAttr("alt")&&imgs.get(i).hasAttr("src")) {
+//                    String imgTitle = imgs.get(i).attr("alt");//图片描述
+//                    String imgUrl = imgs.get(i).attr("src");
+//                    pictureBean.setPictureUrl(imgUrl);
+//                    pictureBean.setPictureTitle(imgTitle);
+//                    list.add(pictureBean);
+//                }
+//            }
             return list;
         } catch (IOException e) {
             e.printStackTrace();
@@ -55,22 +73,37 @@ public class GetPictureAPI {
         List<PictureBean> list = null;
         try {
             Document document = Jsoup.connect(nextUrl).get();
-            Elements imgs = document.getElementsByTag("img");
 
-            if (imgs != null) {
+            Elements liboxElemnets =document.getElementsByClass("libox");
+            if (liboxElemnets.size()>0) {
                 list = new ArrayList<>();
+                return null;
             }
 
-            for (int i = 0; i < imgs.size(); i++) {
+            for (int i = 0; i <liboxElemnets.size() ; i++) {
                 PictureBean pictureBean = new PictureBean(Parcel.obtain());
-                if (imgs.get(i).hasAttr("alt")) {
-                    String imgTitle = imgs.get(i).attr("alt");//图片描述
-                    String imgUrl = imgs.get(i).attr("src");
-                    pictureBean.setPictureUrl(imgUrl);
-                    pictureBean.setPictureTitle(imgTitle);
-                    list.add(pictureBean);
-                }
+                String imgUrl= liboxElemnets.get(i).getElementsByTag("img").attr("src");
+                String imgTitle=liboxElemnets.get(i).getElementsByTag("p").text();
+                pictureBean.setPictureUrl(imgUrl);
+                pictureBean.setPictureTitle(imgTitle);
+                list.add(pictureBean);
             }
+//            Elements imgs = document.getElementsByTag("img");
+//
+//            if (imgs != null) {
+//                list = new ArrayList<>();
+//            }
+//
+//            for (int i = 0; i < imgs.size(); i++) {
+//                PictureBean pictureBean = new PictureBean(Parcel.obtain());
+//                if (imgs.get(i).hasAttr("alt")) {
+//                    String imgTitle = imgs.get(i).attr("alt");//图片描述
+//                    String imgUrl = imgs.get(i).attr("src");
+//                    pictureBean.setPictureUrl(imgUrl);
+//                    pictureBean.setPictureTitle(imgTitle);
+//                    list.add(pictureBean);
+//                }
+//            }
             return list;
 
         } catch (IOException e) {
